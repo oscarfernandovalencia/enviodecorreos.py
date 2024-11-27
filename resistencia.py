4a090429998dd56ee1a44e3731d221a187db37ad
v1rr3eor <osvamail02@gmail.com>
2024-11-27 13:24:52 -0500

    Agrego script de resistencia de materiales

---
+++ resistencia.py
@@ -0,0 +1,17 @@
+import time
+from colorama import init, Fore, Back
+
+print(Back.RED + Fore.GREEN + "INTRODUCCIÓN. EL CONCEPTO DE ESFUERZO" + Fore.RESET + Back.RESET)
+print(Fore.GREEN + "1.1 Introducción")
+print(Fore.GREEN + "1.2 Un breve repaso de los métodos de la estática")
+print(Fore.GREEN + "1.3 Esfuerzos en los elementos de una estructura")
+print(Fore.GREEN + "1.4 Análisis y diseño")
+print(Fore.GREEN + "1.5 Carga axial. Esfuerzo normal")
+print(Fore.GREEN + "1.6 Esfuerzo cortante")
+print(Fore.GREEN + "1.7 Esfuerzo de apoyo en conexiones")
+print(Fore.GREEN + "1.8 Aplicación al análisis y diseño de estructuras sencillas")
+print(Fore.GREEN + "1.9 Método para la solución de problemas")
+print(Fore.GREEN + "1.10 Exactitud numérica")
+print(Fore.GREEN + "1.11 Esfuerzos en un plano oblicuo bajo carga axial")
+
+time.sleep(4)  
